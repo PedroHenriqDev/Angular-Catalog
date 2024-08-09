cf2c7ec3dc5462c3c736181aad1c79c1d38d1300
Pedro Henirique <heroperde@gmail.com>
2024-08-09 18:31:06 -0300

    Created Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CategoryDetailsComponent } from './category-details/category-details.component';
+import { CategoryEditComponent } from './category-edit/category-edit.component';
+import { CategoryNewComponent } from './category-new/category-new.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    CategoryDetailsComponent,
+    CategoryEditComponent,
+    CategoryNewComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
